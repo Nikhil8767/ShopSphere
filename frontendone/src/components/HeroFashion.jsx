23def7f67793f2c939073fe8ac3c9a515e5822ad
Nikhil <nikhilgupta932003@gmail.com>
2025-02-22 03:12:43 +0530

    create the top part or fashionhero

--- frontendone/src/components/HeroFashion.jsx
+++ frontendone/src/components/HeroFashion.jsx
@@ -2,27 +2,34 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const HeroFashion = () => {
-  return (
-    <div className='flex flex-col sm:flex-row border border-gray-400'>
-        {/* this will be our hero left side  */}
-        <div className='text-[#414141]'>
-            <div className='flex items-center gap-2'>
-                <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                <p className='font-medium text-sm md:text-base'>OUR BEST SELLER</p>
+    return (
+        <div className='px-4 sm:px-10 '>
+        <div className='flex flex-col sm:flex-row border border-gray-400'>
+            {/* this will be our hero left side  */}
+            <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+                <div className='text-[#414141]'>
 
-            </div>
-            <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Latest Arrival</h1>
-                <div className='flex item-center gap-2'>
-                    <p className='font-semibold'>SHOP NOW</p>
-                    <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
+                    <div className='flex items-center gap-2'>
+                        <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
+                        <p className='font-medium text-sm md:text-base'>OUR BEST SELLER</p>
+                    </div>
+
+                    <h1 className='text-3xl sm:py-3 lg:text-5xl px-5 leading-relaxed'>"Buy more show your Fashion Sense"</h1>
+
+                    <div className='flex item-center gap-2'>
+                        <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
+                        <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
+                    </div>
+
                 </div>
+            </div>
+
+            {/* hero right side  the image wil be ther */}
+
+            <img src={assets.hero_img} className='w-full sm:w-1/2' alt="" />
         </div>
-
-        {/* hero right side  the image wil be ther */}
-
-        <img src={assets.hero_img} className='w-full sm:w-1/2' alt="" />
-    </div>
-  )
+        </div>
+    )
 }
 
 export default HeroFashion
